ac5a962d7323ab4f7f2f95c10c317be322aff5b8
Samuel Yu <yu.s@etoncollege.org.uk>
2024-09-24 10:35:16 +0100

    Test Commit

--- Challenge 3 - Cities/main.swift
+++ Challenge 3 - Cities/main.swift
@@ -4,8 +4,15 @@
 //
 //  Created by Cormell, David - DPC on 17/09/2024.
 //
+//  Write a program that allows the user to input the name of two cities.
+//
+//  The program should then output the first 4 characters of each city in capital letters, separated by a dash.  For example, London & Madrid would be: LOND-MADR
 
 import Foundation
 
-print("Hello, World!")
+func cityNameCombiner(cityOne: String, cityTwo: String) -> String {
+    var result = String()
+    var editCityOne = String()
+    var editCityTwo = String()
 
+}
